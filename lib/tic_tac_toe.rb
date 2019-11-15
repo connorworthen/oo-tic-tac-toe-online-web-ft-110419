13fa3c7a4f379e65d4866ec81620230d13a02d89
connor worthen <connorw220@protonmail.com>
2019-11-15 22:12:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ class TicTacToe
 
   def turn
     puts "Player #{current_player}, please enter a number 1-9:"
-    input = gets.strip
+    
     index = input_to_index(input)
     cp = current_player
     if valid_move?(index)
